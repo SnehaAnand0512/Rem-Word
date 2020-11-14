34b2896ccd8605a3c37a639be9c25ddf2f3348ba
SA <69071322+stepin104339@users.noreply.github.com>
2020-11-14 09:54:45 +0530

    Create remword.h

---
+++ 3_Implementation/inc/remword.h
@@ -0,0 +1,16 @@
+#ifndef PROJECT_H_INCLUDED
+#define PROJECT_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include"project_main.h"
+
+void login();
+int  menu();
+void add();
+void search();
+void all();
+
+
+#endif
